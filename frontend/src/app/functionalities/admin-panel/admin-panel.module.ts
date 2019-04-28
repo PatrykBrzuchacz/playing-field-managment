0c27fa755c54b2024d420e544f2ed39085b8d74f
PatrykBrzuchacz <patrykbrzuchacz@gmail.com>
2019-04-28 18:38:58 +0200

    add securiy-jwt on frontend - login and register with validation

---
+++ frontend/src/app/functionalities/admin-panel/admin-panel.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserManagmentComponent } from './components/user-managment/user-managment.component';
+
+@NgModule({
+  declarations: [UserManagmentComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminPanelModule { }
