1725f83bc7c24213e07133422066f6a95281c357
PatrykBrzuchacz <patrykbrzuchacz@gmail.com>
2019-04-28 20:11:34 +0200

    add admin-panel

--- frontend/src/app/functionalities/admin-panel/admin-panel.module.ts
+++ frontend/src/app/functionalities/admin-panel/admin-panel.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserManagmentComponent } from './components/user-managment/user-managment.component';
+import { AdminLayoutComponent } from './components/admin-layout/admin-layout.component';
+import { SharedModule } from '@app/shared/shared.module';
+import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 
 @NgModule({
-  declarations: [UserManagmentComponent],
+  declarations: [UserManagmentComponent, AdminLayoutComponent],
   imports: [
-    CommonModule
+    SharedModule,
+    AdminPanelRoutingModule
+  ],
+  providers: [
+
   ]
 })
 export class AdminPanelModule { }
