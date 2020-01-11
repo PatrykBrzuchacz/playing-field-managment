7bc42482ac4d901d8da64d6692c5b758f98b1be9
patrykbrzuchacz <patrykbrzuchacz@gmail.com>
2020-01-11 22:29:30 +0100

    v1.2

--- src/main/java/engineersthesis/playingfieldmanagment/modules/security/config/WebSecurityConfig.java
+++ src/main/java/engineersthesis/playingfieldmanagment/modules/security/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -18,17 +20,23 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private final String[] PUBLIC_GET_PATHS = {"/api/users", "/api/friends/{senderId}", "/api/searchByLocation",
             "/api/searchByCity", "/api/playingField/availabilities/{senderId}", "/api/playingField/{senderId}/setup",
             "/api/match/{senderId}/teams", "/api/users/{senderId}", "/api/users/getLoggedUserWithAvatar", "/api/match/users/{senderId" +
             "}/getAllMatches", "/api/users/{senderId}/username",
-            "/api/matches/{id}/invites","/api/matches/users/{id}/getAllMatches", "/api/playingField/{id}/getRate","/*"};
+            "/api/matches/{id}/invites","/api/matches/users/{id}/getAllMatches", "/api/playingField/{id}/getRate",
+            "/**"};
 
     private final String[] PUBLIC_POST_PATHS = {"/api/users/signup", "/api/users", "/api/login",
             "/api/workerRequests/worker/signup", "/api/assignToWorkerAndRegister",
@@ -52,7 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, PUBLIC_GET_PATHS).permitAll()
                 .antMatchers(HttpMethod.POST, PUBLIC_POST_PATHS).permitAll()
-                .antMatchers("/**").authenticated()
+//                .antMatchers("/**").authenticated()
                 .and()
                 .addFilterBefore(new JwtLoginFilter("/api/login", authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
@@ -66,7 +74,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(11);
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
 //    @Bean
 //    public CorsConfigurationSource corsConfigurationSource() {
 //        CorsConfiguration config = new CorsConfiguration();
